ded5724f71f8a2491fb3ea322037c5ece4b01c49
Abdullahi <abdullahiabdirashid@gmail.com>
2018-10-20 11:14:43 -0500

    Updates

---
+++ precache-manifest.6449c3d6bdad6acfa36884b4300afeee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "402eee131f61c675f8cb9590936c35ac",
+    "url": "/clickygame/static/media/background.402eee13.png"
+  },
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "d6a2f6d6bd4f4f9e92d2",
+    "url": "/clickygame/static/js/main.d6a2f6d6.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/clickygame/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "d6a2f6d6bd4f4f9e92d2",
+    "url": "/clickygame/static/css/main.94dd5ac4.chunk.css"
+  },
+  {
+    "revision": "e753094b82404d0db00be19659c105e0",
+    "url": "/clickygame/index.html"
+  }
+];
